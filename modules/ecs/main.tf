2a21a71087c01e285705313b333492b0d6380599
sasender <sasender123@gmail.com>
2025-02-09 09:32:47 +0000

    adding-new-repo

---
+++ modules/ecs/main.tf
@@ -0,0 +1,194 @@
+####################################################
+# Create an IAM role - ecsTaskExecutionRole  
+####################################################
+data "aws_iam_policy" "ecsTaskExecutionRolePolicy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+data "aws_iam_policy_document" "ecsExecutionRolePolicy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.ecsExecutionRolePolicy.json
+}
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionPolicy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = data.aws_iam_policy.ecsTaskExecutionRolePolicy.arn
+}
+
+####################################################
+# Create cloudWatch Log Group
+####################################################
+resource "aws_cloudwatch_log_group" "log" {
+  name              = "/${var.ecs_cluster_name}/simplenodejsapp"
+  retention_in_days = 14
+}
+
+####################################################
+# Create an ECS cluster
+####################################################
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = var.ecs_cluster_name
+
+  tags = merge(var.common_tags, {
+    Name = "${var.naming_prefix}-ecs-cluster"
+  })
+}
+
+####################################################
+# Create an ECS capacity Provider
+####################################################
+resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
+  name = "capacity_provider"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = var.auto_scaling_group_arn
+    managed_termination_protection = "ENABLED"
+
+    managed_scaling {
+      maximum_scaling_step_size = 5
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 100
+    }
+  }
+}
+
+####################################################
+# Create an ECS Cluster capacity Provider
+####################################################
+resource "aws_ecs_cluster_capacity_providers" "ecs_cluster_capacity_provider" {
+  cluster_name       = aws_ecs_cluster.ecs_cluster.name
+  capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
+}
+
+
+####################################################
+# Create an ECS Task Definition
+####################################################
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+  family             = "my-ecs-task"
+  network_mode       = "bridge"
+  execution_role_arn = aws_iam_role.ecsTaskExecutionRole.arn
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+
+  container_definitions = jsonencode([
+    {
+      name      = "simple-nodejs-app"
+      image     = "865964247420.dkr.ecr.us-east-1.amazonaws.com/nginx/nginx:1.0"
+      cpu       = 200
+      memory    = 200
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 0
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.log.name
+          "awslogs-region"        = var.aws_region
+          "awslogs-stream-prefix" = "simplenodejsapp"
+        }
+      }
+    }
+  ])
+}
+
+
+####################################################
+# Define the ECS service that will run the task
+####################################################
+resource "aws_ecs_service" "ecs_service" {
+  name                               = "my-ecs-service"
+  cluster                            = aws_ecs_cluster.ecs_cluster.id
+  task_definition                    = aws_ecs_task_definition.ecs_task_definition.arn
+  desired_count                      = 4
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 100
+
+  ## Spread tasks evenly accross all Availability Zones for High Availability
+  ordered_placement_strategy {
+    type  = "spread"
+    field = "attribute:ecs.availability-zone"
+  }
+
+  ## Make use of all available space on the Container Instances
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "memory"
+  }
+
+  triggers = {
+    redeployment = timestamp()
+  }
+
+  capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+    weight            = 100
+  }
+
+  load_balancer {
+    target_group_arn = var.alb_target_group_arn
+    container_name   = "simple-nodejs-app"
+    container_port   = 80
+  }
+}
+
+####################################################
+# Define the ECS service auto scaling
+####################################################
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 50
+  min_capacity       = 2
+  resource_id        = "service/${aws_ecs_cluster.ecs_cluster.name}/${aws_ecs_service.ecs_service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy_memory" {
+  name               = "${var.naming_prefix}-memory-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value = 80
+  }
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
+  name               = "${var.naming_prefix}-cpu-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value = 80
+  }
+}
